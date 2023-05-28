922d4a7a3661e6be75fd904cf9232c1dce3fe803
Lanzhu <63757756+Zeistxz@users.noreply.github.com>
2023-05-27 22:06:57 -0500

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,13 +1,18 @@
  <!-- Conexion principal de la pagina principal hacia la base de datos -->
 <?php 
-$contrasena = "";
-$usuario = "root";
+$contrasena = "AVNS_kKSkflCp2xGgajXHnqB";
+$usuario = "doadmin";
 $nombre_bd = "park";
+$host= "db-mysql-nyc1-28475-do-user-14089120-0.b.db.ondigitalocean.com";
+$port= 25060;
+$sslmode= "REQUIRED";
 
 try {
 	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$nombre_bd,
+		"mysql:host=$host;
+		port=$port;
+		dbname=$nombre_bd;
+		sslmode=$sslmode",
 		$usuario,
 		$contrasena,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
